32f745752555714cca7303ae704255c8545d3231
Maxim <yalagin@gmail.com>
2020-05-27 13:12:14 +0300

    Setting up config before php artisan ui bootstrap --auth

--- config/app.php
+++ config/app.php
@@ -227,6 +227,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // custom
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
+
     ],
 
 ];
