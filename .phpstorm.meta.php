f8c9a0915f47bdbd0ebd7b74925f4598ab1cec4c
Maxim <yalagin@gmail.com>
2020-06-15 20:42:51 +0300

    Add money bundle

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-06-12 18:30:08.
+    * Generated on 2020-06-15 17:35:15.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.13.0 on 2020-06-12 18:30:07.
+ * Generated for Laravel 7.13.0 on 2020-06-15 17:35:15.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
